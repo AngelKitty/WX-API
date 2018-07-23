33b5fce969455f6f070e8d0c4d6a45bf64d701ea
AngelKitty <873284962@qq.com>
2018-07-23 20:11:37 +0800

    微信API与电影网站对接

--- wx_api.php
+++ wx_api.php
@@ -1,71 +1,148 @@
-<?php
-define("TOKEN", "AngelKitty"); //定义常量
-$wechatObj = new wechatCallbackapiTest();//实例化类                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-if (isset($_GET['echostr'])) { //如果随机字符串存在
-    $wechatObj->valid(); //执行wechatObj类下的valid函数
-}else{
-    $wechatObj->responseMsg(); //如果未得到随机字符串，执行wechatObj类下的responseMsg函数
-}
+﻿<?php
+/**
+ *
+ * wechat php test
+ */
 
-class wechatCallbackapiTest  //定义类
+//define your token
+//weixinabc是一个token,是一个令牌
+define("TOKEN", "AngelKitty");
+$wechatObj = new wechatCallbackapiTest();
+
+$wechatObj->responseMsg();
+//$wechatObj->valid();
+//exit;
+
+class wechatCallbackapiTest
 {
-    public function valid()  //定义valid函数
+	public function valid()
     {
-        $echoStr = $_GET["echostr"];  //定义变量$echoStr 为获得的字符串
-        if($this->checkSignature()){  //调用当前类里的chekSignature函数
-            echo $echoStr;  //输出echoStr的值
-            exit;
+        $echoStr = $_GET["echostr"];
+
+
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
         }
     }
 
-    private function checkSignature()  //定义checkSignature函数
+
+    public function responseMsg()
     {
-        $signature = $_GET["signature"];  //定义变量signature为获得的signature
-        $timestamp = $_GET["timestamp"];  //获取时间戳
-        $nonce = $_GET["nonce"];          //获取随机数
 
-        $token = TOKEN; //获得token
-        $tmpArr = array($token, $timestamp, $nonce);//定义数组tmpArr包含token值、时间戳、随机数
-        sort($tmpArr, SORT_STRING);  //对数组进行升序排列
-        $tmpStr = implode( $tmpArr );  //将数组组合为字符串
-        $tmpStr = sha1( $tmpStr );  //计算字符串的sha1散列
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-        if( $tmpStr == $signature ){ //如果计算的散列与获取的加密签名一致
-            return true;  //返回真
-        }else{
-            return false;  //不一致，返回假
+
+		if (!empty($postStr)){
+
+                libxml_disable_entity_loader(true);
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+
+				$event = $postObj->Event;			
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";    
+				
+
+
+				switch($postObj->MsgType)
+				{
+					case 'event':
+
+						if($event == 'subscribe')
+						{
+						//关注后的回复
+												$contentStr = "欢迎来到樱姐姐的花园！发送你想看的电影、电视剧【名字】于平台，即可获取最新电影,上万部院线大片资源,可能是微信上最有温度的电影平台。您的到来使我们最大的荣幸！";
+
+
+							$msgType = 'text';
+							$textTpl = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+							echo $textTpl;
+
+						}
+						break;
+					case 'text':
+						{
+							$newsTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[news]]></MsgType>
+							<ArticleCount>1</ArticleCount>
+							<Articles>
+							<item>
+							<Title><![CDATA[%s]]></Title> 
+							<Description><![CDATA[%s]]></Description>
+							<PicUrl><![CDATA[%s]]></PicUrl>
+							<Url><![CDATA[%s]]></Url>
+							</item>							
+							</Articles>
+							</xml>";	
+ 						if($keyword<>"")
+						{
+										$title = '您要看的《'.$keyword.'》,给您找到以下结果：';
+										
+										$des1 ="";
+										//图片地址
+										$picUrl1 ="https://i.loli.net/2018/05/03/5aeb0eef73838.jpg";
+										//跳转链接
+										$url="http://vip.qiluedu.net/wxsea.php?wd=".$keyword;
+
+										$resultStr= sprintf($newsTpl, $fromUsername, $toUsername, $time, $title, $des1, $picUrl1, $url) ;
+									
+										echo $resultStr; 	
+						}
+												$contentStr = " \r\n 输入电影名如：画江湖之不良人 即可在线观看！\r\n ";
+
+
+							$msgType = 'text';
+							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+							echo $resultStr;
+						}					
+						break;
+					default:
+						break;
+				}						
+
+        }else {
+        	echo "你好！欢迎关注樱姐姐的微信公众号";
+        	exit;
         }
     }
-
-    public function responseMsg()  //定义函数 responseMsg
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];  //首先接收上述原始POST数据
-
-        if (!empty($postStr)){  //如果接收到的数据不为空
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);  //将数据载入对象中，对象名为SimpleXMLElement，将CDATA合并为文本节点
-            $fromUsername = $postObj->FromUserName;  //取得XML数据包中FromUserName的值，赋给变量fromUsername
-            $toUsername = $postObj->ToUserName;  //取得XML数据包中ToUsername的值，赋给变量toUsername
-            $keyword = trim($postObj->Content);  //取得XML数据包中content的值，并移除字符串两侧的值
-            $time = time();  //取得XML数据包中的time的值
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";//将变量依次赋给新的XML变量textTpl，注意里边将to和from调换 为什么多个%s？
-            if($keyword == "?" || $keyword == "？")  //判断用户发送的关键词是不是问号
-            {
-                $msgType = "text";  //数据类型为文本方式text
-                $contentStr = date("Y-m-d H:i:s",time());  //回复的内为未当前时间
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);  //sprintf是把前边字符串里的%作为参数传递，第一个textTpl是前边定义的一串xml，接下来5个参数是要填充进XML中进行替换的内容
-                echo $resultStr;  //输出XML，这时候微信服务器就会获得结果，并展示给用户
-            }
-        }else{  //如果接收到的数据是空的
-            echo "";  //返回一个空值即可
-            exit;
+		
+	private function checkSignature()
+	{
+        // you must define TOKEN by yourself
+        if (!defined("TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
         }
-    }
+        
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
+
 ?>
